3a42617109dad24fbef64941dfa253a933223562
fatih <muhammedfatihakyol@gmail.com>
2020-12-18 01:23:46 +0300

    project-builded-ready-to-deploy-netlify

---
+++ build/precache-manifest.9f05bb5c571997cc03f63d78f2df369c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b40bf85c3af0bdfa56e70557ebc833b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d53270e640ff0cc7cdc9",
+    "url": "/static/css/main.def4889f.chunk.css"
+  },
+  {
+    "revision": "e355fa96012d16333ec0",
+    "url": "/static/js/2.a4d8a513.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a4d8a513.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d53270e640ff0cc7cdc9",
+    "url": "/static/js/main.79036379.chunk.js"
+  },
+  {
+    "revision": "eac892d70cbcd7e3df75",
+    "url": "/static/js/runtime-main.55a906e9.js"
+  },
+  {
+    "revision": "0b3d821493d5748d829c852bbd6d292d",
+    "url": "/static/media/all.0b3d8214.png"
+  },
+  {
+    "revision": "587e040f20259792094901ca9739669e",
+    "url": "/static/media/mobile-inline-image.587e040f.png"
+  },
+  {
+    "revision": "a48333d43c19612ed61987fa5dbff3c5",
+    "url": "/static/media/mobile.a48333d4.jpg"
+  },
+  {
+    "revision": "1c3ce9479f746a5916e9ee9da067ee11",
+    "url": "/static/media/tr-bg.1c3ce947.jpg"
+  },
+  {
+    "revision": "4d813cc7828f57f1f261a99408f99fa9",
+    "url": "/static/media/tr-en-bg.4d813cc7.jpg"
+  },
+  {
+    "revision": "77994a67327ba957dfd880e33a91f041",
+    "url": "/static/media/tv.77994a67.png"
+  },
+  {
+    "revision": "3e55c56205a89c859fed005ae1729faa",
+    "url": "/static/media/video-all.3e55c562.m4v"
+  },
+  {
+    "revision": "3cb12db7c672b33735006480a455ec72",
+    "url": "/static/media/video-tv.3cb12db7.m4v"
+  }
+]);
